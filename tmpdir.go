7c08a95db1c453277e90d1cbfe6139c4d4f9210f
zp4rker <iam@zp4rker.com>
2022-01-10 11:38:36 +1100

    now uses a better looking id

--- tmpdir.go
+++ tmpdir.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"os"
 )
+
+// func main() {
+// 	os.Mkdir("/tmp/tmpdir/", 0777)
+// 	defer os.RemoveAll("/tmp/tmpdir/")
+
+// 	dir, _ := os.MkdirTemp("/tmp/tmpdir", "tmpdir-")
+
+// 	println(dir)
+// }
 
 func main() {
 	os.Mkdir("/tmp/tmpdir/", 0777)
 	defer os.RemoveAll("/tmp/tmpdir/")
 
-	dir, _ := os.MkdirTemp("/tmp/tmpdir", "tmpdir-")
+	os.Mkdir("/tmp/tmpdir/tmpdir-"+randToken(), 0777)
 
-	println(dir)
+	entries, _ := os.ReadDir("/tmp/tmpdir/")
+	for _, entry := range entries {
+		println(entry.Name())
+	}
+}
+
+func randToken() string {
+	b := make([]byte, 4)
+	rand.Read(b)
+	token := fmt.Sprintf("%x", b)
+	println(token)
+	return token
 }
