9880a5d7c6269766abf0be09722c076104434f15
zp4rker <iam@zp4rker.com>
2022-01-27 15:55:46 +1100

    feat: implement absolute basics

---
+++ sampleapp.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "You found me!")
+	})
+
+	router.Run(":8080")
+}
